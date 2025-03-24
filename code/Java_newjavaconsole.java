d1d2b291bea60601ca7536836095cadc66cac1b1
Gabrielsynapse <gabrielsynapse12@gmail.com>
2025-03-24 19:50:39 -0300

    incluido arquivo FileUtil.java e atualizado .gitignore para os arquivos .class

---
+++ code/Java_newjavaconsole.java
@@ -0,0 +1,14 @@
+package code;
+
+public class Java_newjavaconsole{
+	private static String groupId;
+	private static String artifactId;
+	private static String version;
+
+	public static void main(String[] args){
+		groupId = args[0];
+		artifactId = args[1];
+		version = args[2];
+
+	}
+}
